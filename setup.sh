fcf5091e51a5f9622bce4889fbe6dc476589c89c
tori.netzer <tori.netzer@capgemini.com>
2023-08-28 13:53:51 -0500

    First commit

---
+++ setup.sh
@@ -0,0 +1,16 @@
+rm -rf /Applications/*
+
+#!/bin/zsh
+
+# Install Homebrew if not already installed
+if ! command -v brew &> /dev/null
+then
+    echo "Homebrew not found. Installing..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+brew bundle
+
+npm install --global @thegetty/quire-cli 
+
+quire --version
